e659f6ca2446e9dbe0fbf72cd4a8d7565dda746f
Rustam-R-D-S <https://github.com/Rustam-R-D-S/home-work-51.git>
2024-05-23 17:09:18 +0600

    added interface, function handleChange,input,

--- src/AddTaskForm.tsx
+++ src/AddTaskForm.tsx
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { ChangeEvent, FC } from 'react';
 
-const AddTaskForm = () => {
+interface AddTaskFormProps {
+    currentTask: string;
+    setCurrentTask: (task: string) => void;
+    addTask: () => void;
+}
+
+const AddTaskForm: FC<AddTaskFormProps> = ({ currentTask, setCurrentTask, addTask }) => {
+    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setCurrentTask(e.target.value);
+    };
+
     return (
-        <div>
-            
+        <div className="add-task-form">
+            <input
+                type="text"
+                value={currentTask}
+                onChange={handleChange}
+                placeholder="Enter new task"
+            />
+            <button onClick={addTask}>Add</button>
         </div>
     );
 };
